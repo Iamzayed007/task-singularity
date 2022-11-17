e522a3f249f8ce06f693ae482614ac1e6d9ac377
Iamzayed007 <zhr1720@gmail.com>
2022-11-17 11:03:31 +0600

    update

--- index.js
+++ index.js
@@ -6,8 +6,28 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+async function run(){
+    try {
+        app.get('/article', async (req, res) => {
+            const email = req.query.email;
+            const date = req.query.date;
+
+            const query = { email: email, date: date }
+
+            const cursor = carsCollection.find(query);
+            const cars = await cursor.toArray();
+            res.json(cars);
+        })
+    } catch (error) {
+        
+    }
+}
+
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
-    res.send('surver is running')
+    res.send('server is running')
 })
 
 
